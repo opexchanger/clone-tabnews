1b8c380de07c4cb290f4f9e4961d457c6ec12726
Flavio Neto <flavio.neto@br-mediagroup.com>
2023-11-16 20:43:36 -0300

    ta me tirando esse rebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Home() {
-  return <h1>vsf</h1>;
+  return (
+    <h1>
+      Ninguem saberá o que estava escrito aqui antes de eu fazer esse rebase
+    </h1>
+  );
 }
