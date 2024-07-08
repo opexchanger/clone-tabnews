bc4463e2cf9bd39324b8f7d7b6bb9300a0a6107f
Flavio Neto <flavio.neto@br-mediagroup.com>
2024-07-08 20:33:08 -0300

    add database module

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(request, response) {
-   response.status(200).json({ status: 'OK' });
+import database from "../../../../infra/database";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1+1;");
+  console.log("result :>> ", result);
+  response.status(200).json({ status: "OK" });
 }
