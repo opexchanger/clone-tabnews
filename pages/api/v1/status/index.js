90f727553353c00a63d0a33446e5499faf036d35
Flavio Neto <flavio.neto@br-mediagroup.com>
2024-07-20 13:08:57 -0300

    refactor: use base import

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database";
+import database from "/infra/database";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1+1;");
-  console.log("result :>> ", result);
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log("rows :>> ", result.rows);
   response.status(200).json({ status: "OK" });
 }
